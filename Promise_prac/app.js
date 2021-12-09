717309da03ab42a44feba67eb74cb764f207f4a7
Endmostcamera32 <Sohrab.ram95@hotmail.com>
2021-12-09 10:59:45 -0800

    The Last app in the list update

--- Promise_prac/app.js
+++ Promise_prac/app.js
@@ -34,3 +34,66 @@ jsDevs.map(d => {
     delete d.fullName;
     return d;
 }).reduce((acc, dev) => acc + dev.salary, 0) / jsDevs.length;
+
+
+
+
+
+
+// the Last App version 1
+
+const fs = require('fs');
+const parser = require('parse-json');
+
+
+
+
+
+new Promise
+((resolve, rejects) =>
+    {
+        return fs.readFile ("./supply.txt",(err, data) => {
+            if (err) {
+                rejects(err);
+            } else {
+                resolve(data);
+            }
+        });
+    }
+)
+.then(data => {
+ let theCofeeArray = [];
+ let theCofees = data.toString();
+ let theNewCofees =  theCofees.split(/\r?\n/);
+ console.log(typeof(theCofees));
+ console.log(theNewCofees);
+    function viewAllSupply(kindOfCofee) {
+        let anArrayWithOnlyTheKindOFCofeeInput = theNewCofees.filter(m => m === kindOfCofee)
+        return anArrayWithOnlyTheKindOFCofeeInput.length;
+    }
+console.log(viewAllSupply("blonde"));
+})
+.then(data => {
+    function addSupply(kindOfCofee) {
+        fs.appendFile("./supply.txt",`\n${kindOfCofee}`, cb2);
+    }
+
+addSupply("yeye");
+})
+
+.catch(err => {
+console.log(err);
+});
+
+
+
+
+
+function cb2(err, data) {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log("Good job")
+    }
+}
+
